d2352be01c126d5640085f6df1a43414f265686b
Gareth Bryce <gareth@Gareths-MacBook-Pro.local>
2025-01-01 16:50:53 +0000

    correct bad package

--- retry/retry_test.go
+++ retry/retry_test.go
@@ -8,9 +8,7 @@ import (
 )
 
 func TestFailedRetryable(t *testing.T) {
-
 	retry := Retryable(func(ctx context.Context) (string, error) { return "", errors.New("failed") }, 5, 1)
-
 	_, err := retry(context.Background())
 
 	assert.EqualError(t, err, "failed")
